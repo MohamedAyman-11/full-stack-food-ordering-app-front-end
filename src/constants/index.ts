3db64fcc1f4b498b02ab2957d16a24958453e16e
MohamedAyman-11 <mhmd.ayman.0101@gmail.com>
2026-08-10 02:33:01 +0300

    feat(Contants): Add Initial Constants

---
+++ src/constants/index.ts
@@ -0,0 +1,33 @@
+export enum Directions {
+  RTL = "rtl",
+  LTR = "ltr",
+}
+
+export enum Languages {
+  ENGLISH = "en",
+  ARABIC = "ar",
+}
+
+export enum Routes {
+  ROOT = "/",
+  MENU = "menu",
+  ABOUT = "about",
+  CONTACT = "contact",
+  AUTH = "auth",
+  CART = "cart",
+  PROFILE = "profile",
+  ADMIN = "admin",
+}
+
+export enum Pages {
+  LOGIN = "signin",
+  Register = "signup",
+  FORGOT_PASSWORD = "forgot-password",
+  RESET_PASSWORD="reset-password",
+  CATEGORIES = "categories",
+  MENU_ITEMS = "menu-items",
+  USERS = "users",
+  ORDERS = "orders",
+  NEW = "new",
+  EDIT = "edit",
+}
